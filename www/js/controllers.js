d62c76d4d24c479326e4139422122a57c9b5f530
Adam Barnhard <admbarn@gmail.com>
2014-11-09 14:59:49 -0600

    added stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -74,7 +74,7 @@
     $scope.cardSwipedLeft = function(index){
       console.log('LEFT SWIPE');
       console.log(index);
-      $http.post('http://friendswipe-php.herokuapp.com', {choice: 'ignore', sender:$rootScope.myFacebookId, recipient:1});
+      // $http.post('http://friendswipe-php.herokuapp.com', {choice: 'ignore', sender:$rootScope.myFacebookId, recipient:1});
       //$scope.addCard();
     };
     $scope.cardSwipedRight = function(index){
